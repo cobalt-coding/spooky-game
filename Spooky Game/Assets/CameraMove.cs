c91cbc39b6a473f0bcb153238f16a8eae8034d7e
Junha Lee <junhalee02@gmail.com>
2017-09-30 21:16:17 -0700

    First Person Controller

---
+++ Spooky Game/Assets/CameraMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    Vector2 mouseLook;
+    Vector2 smoothV;
+    public float sensitivity = 5.0f;
+    public float smooth = 2.0f;
+
+    public GameObject player;
+
+    void Update()
+    {
+        Vector2 md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        md = Vector2.Scale(md, new Vector2(sensitivity * smooth, smooth * sensitivity));
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1.0f / smooth);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1.0f / smooth);
+        mouseLook += smoothV;
+
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);
+
+        if (Input.GetAxis("Vertical") != 0)
+            transform.position = new Vector3(transform.position.x, Mathf.Sin(Time.fixedTime * 15) / 10 + 2.2f, transform.position.z);
+    }
+
+
+}
---
+++ Spooky Game/Assets/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 6.0f;
+    public float gravity = 20.0f;
+    private Vector3 moveDir = Vector3.zero;
+
+    public new Camera camera;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+
+        #region Movement Code
+
+        CharacterController charController = GetComponent<CharacterController>();
+        if (charController.isGrounded)
+        {
+            moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDir = transform.TransformDirection(moveDir);
+            moveDir *= speed;
+        }
+        moveDir.y -= gravity * Time.deltaTime;
+        charController.Move(moveDir * Time.deltaTime);
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+
+        #endregion
+
+    }
+
+}
